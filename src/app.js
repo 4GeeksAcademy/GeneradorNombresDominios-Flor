fddbed9d5c14dda756c9c123bad1ae753bff9892
Florencia <florenciamayaumpierrez@gmail.com>
2024-10-24 14:38:04 +0000

    Generador de nombres de dominios

--- src/app.js
+++ src/app.js
@@ -1,11 +1,41 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+// /* eslint-disable */
+// import "bootstrap";
+// import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+// window.onload = function() {
+let pronoun = ["the", "our"];
+let adj = ["great", "big"];
+let noun = ["jogger", "racoon", "telecom", "puedes"];
+let extensions = [
+  `es`,
+  `uy`,
+  `ve`,
+  `ar`,
+  "com",
+  `co`,
+  `pe`,
+  `it`,
+  `mx`,
+  `br`,
+  `py`
+];
+let dominios = [];
+pronoun.forEach(start => {
+  adj.forEach(mid => {
+    noun.forEach(last => {
+      extensions.forEach(ex => {
+        let domain = `${start}${mid}${last}.${ex}`;
+        if (last.endsWith(ex)) {
+          let domainHack = `${start}${mid}${last.slice(
+            0,
+            -1 * ex.length
+          )}.${ex}`;
+          dominios.push(domainHack);
+        } else {
+          dominios.push(domain);
+        }
+        console.log(dominios);
+      });
+    });
+  });
+});
