42d90805ae3c673fed806b4f1a70cdfd44b5f27b
Florencia <florenciamayaumpierrez@gmail.com>
2024-10-31 22:03:47 +0000

    Generador de Nombres de dominios

--- src/app.js
+++ src/app.js
@@ -5,37 +5,16 @@
 // window.onload = function() {
 let pronoun = ["the", "our"];
 let adj = ["great", "big"];
-let noun = ["jogger", "racoon", "telecom", "puedes"];
-let extensions = [
-  `es`,
-  `uy`,
-  `ve`,
-  `ar`,
-  "com",
-  `co`,
-  `pe`,
-  `it`,
-  `mx`,
-  `br`,
-  `py`
-];
+let noun = ["jogger", "racoon"];
+
 let dominios = [];
+
 pronoun.forEach(start => {
   adj.forEach(mid => {
     noun.forEach(last => {
-      extensions.forEach(ex => {
-        let domain = `${start}${mid}${last}.${ex}`;
-        if (last.endsWith(ex)) {
-          let domainHack = `${start}${mid}${last.slice(
-            0,
-            -1 * ex.length
-          )}.${ex}`;
-          dominios.push(domainHack);
-        } else {
-          dominios.push(domain);
-        }
-        console.log(dominios);
-      });
+      console.log(`${start}${mid}${last}.com`);
+      dominios.push(`${start}${mid}${last}.com`);
     });
   });
 });
+console.log(dominios);
